c9a35b62a98a1dd80b12195a91192c8416ae9fe4
Eduard <ed.mihalev@gmail.com>
2021-02-26 09:31:33 +0300

    Базовая маршрутизация, страница страница логина

--- src/router/index.js
+++ src/router/index.js
@@ -6,18 +6,17 @@ Vue.use(VueRouter)
 
 const routes = [
     {
-        path: '/',
-        name: 'Home',
-        component: Home,
+        path: '/login',
+        name: 'Login',
+        component: () => import('../views/LoginPage.vue'),
     },
     {
-        path: '/about',
-        name: 'About',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () =>
-            import(/* webpackChunkName: "about" */ '../views/About.vue'),
+        path: '/',
+        name: 'Home',
+        meta: {
+            isProtected: true,
+        },
+        component: Home,
     },
 ]
 
@@ -25,6 +24,14 @@ const router = new VueRouter({
     mode: 'history',
     base: process.env.BASE_URL,
     routes,
+})
+
+router.beforeEach((to, from, next) => {
+    if (to.meta && to.meta.isProtected) {
+        return next('/login')
+    }
+
+    next()
 })
 
 export default router
---
+++ src/views/LoginPage.vue
@@ -0,0 +1,40 @@
+<template lang="pug">
+  .page
+    form(@submit.prevent id="loginForm")
+      input(
+          type="text"
+          v-model.trim="login"
+          id="loginForm__login"
+          name="loginForm__login"
+        ).input
+      input(
+          type="password"
+          v-model.trim="password"
+          id="loginForm__password"
+          name="passwordForm__login"
+        ).input
+      button(type="button" @click="loginHandler").btn Вход
+</template>
+
+<script>
+export default {
+    name: 'LoginPage',
+
+    data() {
+        return {
+            login: null,
+            password: null,
+        }
+    },
+
+    methods: {
+        serializeForm() {
+            return {
+                login: this.login,
+                password: this.password,
+            }
+        },
+        loginHandler() {},
+    }
+}
+</script>
